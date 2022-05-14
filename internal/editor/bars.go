2cc97dee1c05967e83f329cc92eceafa50ffc86d
DeeStarks <danielonoja246@gmail.com>
2022-05-14 12:07:53 +0100

    chore: changed title color

--- internal/editor/bars.go
+++ internal/editor/bars.go
@@ -11,18 +11,17 @@ import (
 func (e *Editor) showBars(filename string) { // Display title, numbering... bars
 	filename = fmt.Sprintf("•• %s ••", filename)
 	sw, sh := e.screen.GetScreen().Size() // Get screen size
+	style := utils.CreateStyle(tcell.ColorReset, tcell.ColorDimGray)
 
 	// Title bar
-	tstyle := utils.CreateStyle(tcell.ColorReset, tcell.ColorAntiqueWhite)
-	e.screen.DrawBox(0, 0, sw, 0, "", true, false, true, tcell.StyleDefault)
-	e.screen.DrawText((sw/2)-(len(filename)/2)-1, 0, (sw/2)+(len(filename)/2)+1, 0, filename, tstyle)
+	e.screen.DrawBox(0, 0, sw, 0, "", true, false, false, tcell.StyleDefault)
+	e.screen.DrawText((sw/2)-(len(filename)/2)-1, 0, (sw/2)+(len(filename)/2)+1, 0, filename, style)
 
 	// Draw line numbering side bar
 	nw, nh := 6, sh
-	nstyle := utils.CreateStyle(tcell.ColorReset, tcell.ColorDimGray)
 	e.screen.DrawBox(0, 1, nw, nh, "", true, false, false, tcell.StyleDefault)
 	count := sh
 	for i := 1; i <= count; i++ {
-		e.screen.DrawText(2, i, nw, i+1, strconv.Itoa(i), nstyle)
+		e.screen.DrawText(2, i, nw, i+1, strconv.Itoa(i), style)
 	}
 }
