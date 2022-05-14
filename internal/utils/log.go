4a7f96308892f3ad4d53481135ef40360f391112
DeeStarks <danielonoja246@gmail.com>
2022-05-14 12:06:53 +0100

    feat: implement logger

---
+++ internal/utils/log.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/DeeStarks/lime/internal/constants"
+)
+
+func LogMessage(message string, args ...interface{}) {
+	message = fmt.Sprintf("%v: %s\n", time.Now().Format("2006-01-02 15:04:05"), fmt.Sprintf(message, args...))
+	// Read the log file
+	logFile, err := ioutil.ReadFile(constants.LogFileName)
+	if err != nil {
+		// File does not exist
+		ioutil.WriteFile(constants.LogFileName, []byte(message), 0644)
+		return
+	}
+	// Append the message to the log file
+	ioutil.WriteFile(constants.LogFileName, append(logFile, []byte(message)...), 0644)
+}
