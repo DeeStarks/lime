e304c285cd4732810108b9d9d169d3c9afd53328
DeeStarks <danielonoja246@gmail.com>
2022-06-20 00:05:07 +0100

    clean: log messages

--- internal/utils/file.go
+++ internal/utils/file.go
@@ -3,6 +3,5 @@ package utils
 import "path/filepath"
 
 func GetFileExtension(fileName string) string {
-	LogMessage("GetFileExtension: " + filepath.Ext(fileName))
 	return filepath.Ext(fileName)
 }
